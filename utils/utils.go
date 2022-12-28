29ed15cc54c244e7513b8fedc47991326a68d754
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2022-12-28 19:29:38 -0300

    FEA : add linear algebra utils

--- utils/utils.go
+++ utils/utils.go
@@ -25,3 +25,29 @@ func DirectionFromString(direction string) string {
 func CreateRandomColor() [3]int {
 	return [3]int{rand.Intn(255), rand.Intn(255), rand.Intn(255)}
 }
+
+func CreateMatrixesOfRotation() [4][2][2]int {
+	return [4][2][2]int{
+		{{1, 0}, {0, 1}},
+		{{0, -1}, {1, 0}},
+		{{-1, 0}, {0, -1}},
+		{{0, 1}, {-1, 0}},
+	}
+}
+
+func TransformVector(tMatrix [2][2]int, x int, y int) (int, int) {
+	return tMatrix[0][0]*x + tMatrix[0][1]*y, tMatrix[1][0]*x + tMatrix[1][1]*y
+}
+
+func TransformMatrix(matrix [2][2]int, tMatrix [2][2]int) [2][2]int {
+	var transformedMatrix [2][2]int
+	for i := range matrix {
+		var vector [2]int
+		x, y := TransformVector(tMatrix, matrix[i][0], matrix[i][1])
+		vector = [2]int{x, y}
+		transformedMatrix[i] = vector
+	}
+	return transformedMatrix
+}
+
+var MatrixesOfRotation = CreateMatrixesOfRotation()
