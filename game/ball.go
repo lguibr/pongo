a3298c283284e79b0c9f844725ace0486a645841
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2023-01-14 18:08:21 -0300

    ENH : improve collision with paddles and cells

--- game/ball.go
+++ game/ball.go
@@ -1,8 +1,8 @@
 package game
 
 import (
-	"encoding/json"
 	"fmt"
+	"math"
 	"math/rand"
 
 	"github.com/lguibr/pongo/utils"
@@ -38,15 +38,32 @@ func (b *Ball) CollideCells() {
 	if row < 0 || row > b.Canvas.GridSize-1 || col < 0 || col > b.Canvas.GridSize-1 {
 		return
 	}
+
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			surroundingRow, surroundingCol := row+i, col+j
+
+			if surroundingRow < 0 || surroundingRow > b.Canvas.GridSize-1 || surroundingCol < 0 || surroundingCol > b.Canvas.GridSize-1 {
+				continue
+			}
+
+			ballInterceptsCell := b.BallInterceptCellIndex(surroundingRow, surroundingCol)
+
+			if ballInterceptsCell {
+				t := b.Canvas.Grid[surroundingRow][surroundingCol].Data.Type
+				if t == utils.CellTypes["Brick"] {
+					b.handleCollideBrick([2]int{row, col}, [2]int{surroundingRow, surroundingCol})
+				}
+				if t == utils.CellTypes["Block"] {
+					b.handleCollideBlock([2]int{row, col}, [2]int{surroundingRow, surroundingCol})
+				}
+			}
+		}
+	}
+
 	newIntersectedCell := [2]int{row, col}
+
 	if newIntersectedCell != b.interceptingCell {
-		t := b.Canvas.Grid[row][col].Data.Type
-		if t == utils.CellTypes["Brick"] {
-			b.handleCollideBrick(b.interceptingCell, newIntersectedCell)
-		}
-		if t == utils.CellTypes["Block"] {
-			b.handleCollideBlock(b.interceptingCell, newIntersectedCell)
-		}
 		b.interceptingCell = newIntersectedCell
 	}
 }
@@ -107,75 +124,20 @@ func (ball *Ball) GetIntersectedIndices(grid Grid) (x, y int) {
 }
 
 func (ball *Ball) CollidePaddle(paddle *Paddle) {
-	collisionDetectors := [4]func(*Paddle) bool{
-		ball.CollideOnRightPaddle,
-		ball.CollideOnBottomPaddle,
-		ball.CollideOnLeftPaddle,
-		ball.CollideOnTopPaddle,
-	}
-	collisionDetector := collisionDetectors[paddle.Index]
-	collisionDetected := collisionDetector(paddle)
+
+	collisionDetected := ball.BallInterceptPaddles(paddle)
 
 	if collisionDetected {
 		handlers := [4]func(){
 			ball.HandleCollideRight,
-			ball.HandleCollideBottom,
-			ball.HandleCollideLeft,
 			ball.HandleCollideTop,
+			ball.HandleCollideLeft,
+			ball.HandleCollideBottom,
 		}
 		handlerCollision := handlers[paddle.Index]
+
 		handlerCollision()
 	}
-}
-
-func (ball *Ball) CollideOnTopPaddle(paddle *Paddle) bool {
-	collides := ball.Y-ball.Radius <= paddle.Y+paddle.Height &&
-		ball.Y-ball.Radius >= paddle.Y && ball.X >= paddle.X &&
-		ball.X <= paddle.X+paddle.Width
-
-	if collides {
-		fmt.Println("Collide on top paddle")
-	}
-
-	return collides
-
-}
-
-func (ball *Ball) CollideOnBottomPaddle(paddle *Paddle) bool {
-	collides := ball.X-ball.Radius <= paddle.X+paddle.Width &&
-		ball.X-ball.Radius >= paddle.X &&
-		ball.Y >= paddle.Y && ball.Y <= paddle.Y+paddle.Height
-
-	if collides {
-		fmt.Println("Collide on bottom paddle")
-	}
-
-	return collides
-}
-
-func (ball *Ball) CollideOnLeftPaddle(paddle *Paddle) bool {
-	collides := ball.X-ball.Radius <= paddle.X+paddle.Width/2 &&
-		ball.Y+ball.Radius >= paddle.Y-paddle.Height/2 &&
-		ball.Y-ball.Radius <= paddle.Y+paddle.Height/2
-
-	if collides {
-		fmt.Println("Collide on left paddle")
-	}
-
-	return collides
-}
-
-func (ball *Ball) CollideOnRightPaddle(paddle *Paddle) bool {
-	collides := ball.X+ball.Radius >= paddle.X &&
-		ball.X+ball.Radius <= paddle.X+paddle.Width &&
-		ball.Y >= paddle.Y &&
-		ball.Y <= paddle.Y+paddle.Height
-
-	if collides {
-
-		fmt.Println("Collide on right paddle")
-	}
-	return collides
 }
 
 func (ball *Ball) HandleCollideRight() {
@@ -239,8 +201,8 @@ func NewBall(canvas *Canvas, x, y, radius, index int) *Ball {
 	minVelocity := utils.MinVelocity
 
 	cardinalVX := minVelocity + rand.Intn(maxVelocity-minVelocity)
-
 	cardinalVY := minVelocity + rand.Intn(maxVelocity-minVelocity)
+
 	vx, vy := utils.RotateVector(index, -cardinalVX, cardinalVY, 1, 1)
 
 	return &Ball{
@@ -257,12 +219,80 @@ func NewBall(canvas *Canvas, x, y, radius, index int) *Ball {
 		},
 	}
 }
+func (ball *Ball) BallInterceptCellIndex(x, y int) bool {
+	cellSize := ball.Canvas.CellSize
 
-func (b *Ball) ToJson() []byte {
-	ball, err := json.Marshal(b)
-	if err != nil {
-		fmt.Println(err)
-		return []byte{}
+	leftTopX := x * cellSize
+	leftTopY := y * cellSize
+
+	// calculate the position of the bottom right corner of the grid cell
+	bottomRightX := leftTopX + cellSize
+	bottomRightY := leftTopY + cellSize
+
+	// check if the center of the ball is within the boundaries of the grid cell
+	if ball.X > leftTopX &&
+		ball.X < bottomRightX &&
+		ball.Y > leftTopY &&
+		ball.Y < bottomRightY {
+		return true
 	}
-	return ball
+
+	// check if the distance between the center of the ball and the closest point on the grid cell boundary is less than the radius of the ball
+	closestX := math.Min(
+		math.Max(float64(ball.X), float64(leftTopX)),
+		float64(bottomRightX),
+	)
+
+	closestY := math.Min(
+		math.Max(float64(ball.Y), float64(leftTopY)),
+		float64(bottomRightY),
+	)
+
+	distance := utils.Distance(
+		ball.X,
+		ball.Y,
+		int(closestX),
+		int(closestY),
+	)
+
+	return distance < float64(ball.Radius)
+}
+
+func (ball *Ball) BallInterceptPaddles(paddle *Paddle) bool {
+
+	// calculate the position of the top left corner of the grid cell
+	paddleTopLeftX := paddle.X
+	paddleTopLeftY := paddle.Y
+
+	// calculate the position of the bottom right corner of the grid cell
+	paddleBottomRightX := paddleTopLeftX + paddle.Width
+	paddleBottomRightY := paddleTopLeftY + paddle.Height
+
+	// check if the center of the ball is within the boundaries of the grid cell
+	if ball.X > paddleTopLeftX &&
+		ball.X < paddleBottomRightX &&
+		ball.Y > paddleTopLeftY &&
+		ball.Y < paddleBottomRightY {
+		return true
+	}
+
+	// check if the distance between the center of the ball and the closest point on the grid cell boundary is less than the radius of the ball
+	closestX := math.Min(
+		math.Max(float64(ball.X), float64(paddleTopLeftX)),
+		float64(paddleBottomRightX),
+	)
+
+	closestY := math.Min(
+		math.Max(float64(ball.Y), float64(paddleTopLeftY)),
+		float64(paddleBottomRightY),
+	)
+
+	distance := utils.Distance(
+		ball.X,
+		ball.Y,
+		int(closestX),
+		int(closestY),
+	)
+
+	return distance < float64(ball.Radius)
 }
