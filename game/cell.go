ea8b35cf2bcf8f339dacb04cbbe23e80bb6886c1
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2023-01-16 22:48:11 -0300

    FEA : create newCell newBrickData and compare for cell and brickData func

--- game/cell.go
+++ game/cell.go
@@ -9,3 +9,37 @@ type Cell struct {
 	Y    int        `json:"y"`
 	Data *BrickData `json:"data"`
 }
+
+func NewCell(x, y, life int, typeOfCell string) Cell {
+	return Cell{X: x, Y: y, Data: NewBrickData(typeOfCell, life)}
+}
+
+func NewBrickData(typeOfCell string, life int) *BrickData {
+	if typeOfCell == "Brick" && life == 0 {
+		life = 1
+	}
+	if typeOfCell == "Empty" {
+		life = 0
+	}
+	return &BrickData{Type: typeOfCell, Life: life}
+}
+
+func (cell *Cell) Compare(comparedCell Cell) bool {
+	if cell.Data.Type != comparedCell.Data.Type {
+		return false
+	}
+	if cell.Data.Life != comparedCell.Data.Life {
+		return false
+	}
+	return true
+}
+
+func (data *BrickData) Compare(comparedData *BrickData) bool {
+	if data.Type != comparedData.Type {
+		return false
+	}
+	if data.Life != comparedData.Life {
+		return false
+	}
+	return true
+}
---
+++ game/cell_test.go
@@ -0,0 +1,94 @@
+package game
+
+import (
+	"testing"
+)
+
+func TestNewBrickData(t *testing.T) {
+	type NewBrickDataTestCase struct {
+		typeOfCell string
+		life       int
+		expected   *BrickData
+	}
+
+	testCases := []NewBrickDataTestCase{
+		{typeOfCell: "Brick", life: 0, expected: &BrickData{Type: "Brick", Life: 1}},
+		{typeOfCell: "Brick", life: 2, expected: &BrickData{Type: "Brick", Life: 2}},
+		{typeOfCell: "Empty", life: 0, expected: &BrickData{Type: "Empty", Life: 0}},
+		{typeOfCell: "Empty", life: 2, expected: &BrickData{Type: "Empty", Life: 0}},
+	}
+
+	for _, test := range testCases {
+		result := NewBrickData(test.typeOfCell, test.life)
+		if !result.Compare(test.expected) {
+			t.Errorf("Expected %v for typeOfCell %s and life %d, got %v", test.expected, test.typeOfCell, test.life, result)
+		}
+	}
+}
+
+func TestBrickData_Compare(t *testing.T) {
+	type CompareBrickDataTestCase struct {
+		data         *BrickData
+		comparedData *BrickData
+		expected     bool
+	}
+
+	testCases := []CompareBrickDataTestCase{
+		{&BrickData{Type: "Brick", Life: 1}, &BrickData{Type: "Brick", Life: 1}, true},
+		{&BrickData{Type: "Empty", Life: 0}, &BrickData{Type: "Empty", Life: 0}, true},
+		{&BrickData{Type: "Brick", Life: 2}, &BrickData{Type: "Brick", Life: 1}, false},
+		{&BrickData{Type: "Empty", Life: 0}, &BrickData{Type: "Brick", Life: 0}, false},
+	}
+
+	for _, test := range testCases {
+		result := test.data.Compare(test.comparedData)
+		if result != test.expected {
+			t.Errorf("Expected CompareBrickData(%v, %v) to return %v, got %v", test.data, test.comparedData, test.expected, result)
+		}
+	}
+}
+
+func TestCell_Compare(t *testing.T) {
+	type CompareCellsTestCase struct {
+		cell         Cell
+		comparedCell Cell
+		expected     bool
+	}
+
+	testCases := []CompareCellsTestCase{
+		{Cell{X: 0, Y: 0, Data: &BrickData{Type: "Brick", Life: 1}}, Cell{X: 0, Y: 0, Data: &BrickData{Type: "Brick", Life: 1}}, true},
+		{Cell{X: 0, Y: 0, Data: &BrickData{Type: "Brick", Life: 1}}, Cell{X: 0, Y: 0, Data: &BrickData{Type: "Empty", Life: 1}}, false},
+		{Cell{X: 0, Y: 0, Data: &BrickData{Type: "Brick", Life: 1}}, Cell{X: 0, Y: 0, Data: &BrickData{Type: "Brick", Life: 2}}, false},
+		{Cell{X: 0, Y: 0, Data: &BrickData{Type: "Empty", Life: 0}}, Cell{X: 0, Y: 0, Data: &BrickData{Type: "Empty", Life: 0}}, true},
+	}
+
+	for _, test := range testCases {
+		result := test.cell.Compare(test.comparedCell)
+		if result != test.expected {
+			t.Errorf("Expected CompareCells(%v, %v) to return %v, got %v", test.cell, test.comparedCell, test.expected, result)
+		}
+	}
+}
+
+func TestNewCell(t *testing.T) {
+	type NewCellTestCase struct {
+		x          int
+		y          int
+		life       int
+		typeOfCell string
+		expected   Cell
+	}
+
+	testCases := []NewCellTestCase{
+		{0, 0, 0, "Brick", Cell{X: 0, Y: 0, Data: &BrickData{Type: "Brick", Life: 1}}},
+		{1, 2, 3, "Empty", Cell{X: 1, Y: 2, Data: &BrickData{Type: "Empty", Life: 0}}},
+		{4, 5, 2, "Brick", Cell{X: 4, Y: 5, Data: &BrickData{Type: "Brick", Life: 2}}},
+	}
+
+	for _, test := range testCases {
+		result := NewCell(test.x, test.y, test.life, test.typeOfCell)
+		if !result.Compare(test.expected) {
+			t.Errorf("Expected %v, got %v", test.expected, result)
+		}
+	}
+}
