4d819bc1789cea3933f510f8399b123623a7ddd6
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2023-01-18 21:21:54 -0300

    ENH : add more test case to new canvas testing

--- game/canvas_test.go
+++ game/canvas_test.go
@@ -14,14 +14,17 @@ func TestNewCanvas(t *testing.T) {
 	}
 	testCases := []testCase{
 		{0, 0, false},
-		{100, 0, false},
+		{100, 0, true},
 		{0, 8, false},
 		{100, 7, true},
 		{90, 6, false},
+		{10, 100, true},
+		{100, 5, true},
 	}
 	for index, tc := range testCases {
-		if tc.panics {
-			utils.AssertPanics(t, func() { NewCanvas(tc.size, tc.gridSize) }, fmt.Sprintf("Code did not panic on index %d", index))
+		panics, _ := utils.AssertPanics(t, func() { NewCanvas(tc.size, tc.gridSize) }, fmt.Sprintf("- Code did not panic on index %d", index))
+		if panics != tc.panics {
+			t.Errorf("Code did not panic on index %d", index)
 		}
 	}
 }
